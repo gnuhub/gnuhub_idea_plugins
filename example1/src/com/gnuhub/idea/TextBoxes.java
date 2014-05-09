4c0cdce9f51799d482344e43de8bf8d64780c932
gnuhub <gnuhub@gmail.com>
2014-05-09 10:53:30 +0800

    implement actionPorformed method to response the AnActionEvent

--- example1/src/com/gnuhub/idea/TextBoxes.java
+++ example1/src/com/gnuhub/idea/TextBoxes.java
@@ -1,9 +1,18 @@
 package com.gnuhub.idea;
 
 import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
 
 /**
  * Created by stallman on 14-5-9.
  */
 public class TextBoxes extends AnAction {
+    public void actionPerformed(AnActionEvent event) {
+        Project project = event.getData(PlatformDataKeys.PROJECT);
+        String txt= Messages.showInputDialog(project, "What is your name?", "Input your name", Messages.getQuestionIcon());
+        Messages.showMessageDialog(project, "Hello, " + txt + "!\n I am glad to see you.", "Information", Messages.getInformationIcon());
+    }
 }
